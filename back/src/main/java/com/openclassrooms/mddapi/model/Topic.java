b2e38d3e97fd5a44ef87c87eb4b83b79cf0b97e7
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-05-17 19:20:47 +0200

    Feat/config database (#1)
    * Update project with last version
    
    * Add config for database
    
    * Add User entity
    
    * Add Topic entity
    
    * Add Post entity
    
    * Add Comment entity
    
    * Add all repository
    
    * Add description for topic and fix user error

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/Topic.java
@@ -0,0 +1,22 @@
+package com.openclassrooms.mddapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "TOPICS")
+@Data
+public class Topic {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
