60c5649bbb9956d784797e68fa0c872c54dfd1b4
Marie Metay <mariemetay.pro@gmail.com>
2024-05-17 19:00:06 +0200

    Add Post entity

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/Post.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.mddapi.model;
+
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "POSTS")
+@Data
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+
+    private String subject;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreationTimestamp
+    private Date created_at;
+}
