367cab6fcf6693a0bdfbdd1b855ee2a37429791a
Marie Metay <mariemetay.pro@gmail.com>
2024-05-21 13:30:02 +0200

    Add security

---
+++ back/src/main/java/com/openclassrooms/mddapi/service/JWTService.java
@@ -0,0 +1,42 @@
+package com.openclassrooms.mddapi.service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+    
+    private JwtEncoder jwtEncoder;
+    private JwtDecoder jwtDecoder;
+
+    public JWTService(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+    }
+
+    public String generateToken(String email) {
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+            .issuer("self")
+            .issuedAt(now)
+            .expiresAt(now.plus(1, ChronoUnit.DAYS))
+            .subject(email)
+            .build();
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
+        return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+    }
+
+    public String decodeToken(String token) {
+        Jwt jwt = jwtDecoder.decode(token);
+        return jwt.getSubject();
+    }
+}
