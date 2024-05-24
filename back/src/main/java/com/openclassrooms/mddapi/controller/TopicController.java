ca9c3f0e5fbb7478e3cd617790649087efee71c2
Marie Metay <mariemetay.pro@gmail.com>
2024-05-24 17:01:33 +0200

    Add endpoint for subscribe

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.mddapi.model.Topic;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.JWTService;
 import com.openclassrooms.mddapi.service.TopicService;
+import com.openclassrooms.mddapi.service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/topic")
@@ -20,8 +26,16 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
-    public TopicController(TopicService topicService) {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JWTService jwtService;
+
+    public TopicController(TopicService topicService, UserService userService, JWTService jwtService) {
         this.topicService = topicService;
+        this.userService = userService;
+        this.jwtService = jwtService;
     }
 
     @GetMapping("")
@@ -30,6 +44,25 @@ public class TopicController {
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             List<Topic> topics = topicService.getAllTopics();
             return ResponseEntity.ok(topics);
+        } else {
+            return ResponseEntity.status(401).build();
+        }
+    }
+
+    @PutMapping("/subscribe")
+    public ResponseEntity<?> subscribe(HttpServletRequest request, @RequestBody Topic topic) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            String token = bearerToken.substring(7);
+            String email = jwtService.decodeToken(token);
+            User user = userService.getUser(email);
+
+            List<Topic> currentTopicList = user.getTopics();
+            currentTopicList.add(topic);
+            user.setTopics(currentTopicList);
+            userService.updateUser(user);
+            
+            return ResponseEntity.ok(user);
         } else {
             return ResponseEntity.status(401).build();
         }
