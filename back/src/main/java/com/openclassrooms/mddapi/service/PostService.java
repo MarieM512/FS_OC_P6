d9fbc78618ebeacf01c9ff2ea56a8f16e3cadb85
Marie Metay <mariemetay.pro@gmail.com>
2024-05-24 16:06:39 +0200

    Modify get only posts subscribe

--- back/src/main/java/com/openclassrooms/mddapi/service/PostService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/PostService.java
@@ -1,11 +1,13 @@
 package com.openclassrooms.mddapi.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.model.Topic;
 import com.openclassrooms.mddapi.model.User;
 import com.openclassrooms.mddapi.repository.PostRepository;
 
@@ -24,8 +26,28 @@ public class PostService {
         return postRepository.save(post);
     }
 
-    public List<Post> getAllPosts() {
-        return postRepository.findAll();
+    // public List<Post> getAllPosts() {
+    //     return postRepository.findAll();
+    // }
+
+    public List<Post> getPostsSubscribe(List<Topic> topicSub) {
+        List<Post> allPosts = postRepository.findAll();
+
+        ArrayList<Long> topicIdSub = new ArrayList<>();
+        for (int i = 0; i < topicSub.size(); i++) {
+            topicIdSub.add(topicSub.get(i).getId());
+        }
+
+        ArrayList<Post> postSub = new ArrayList<>();
+        for (int i = 0; i < allPosts.size(); i++) {
+            for (int y = 0; y < topicIdSub.size(); y ++) {
+                if (allPosts.get(i).getTopic().getId() == topicIdSub.get(y)) {
+                    postSub.add(allPosts.get(i));
+                }
+            }
+        }
+
+        return postSub;
     }
 
     public Post getPostById(Long id) {
