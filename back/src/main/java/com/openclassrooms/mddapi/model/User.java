2683444b7a280f2f167b6a720d048ad486c678aa
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-05-24 12:08:13 +0200

    Feat/post (#3)
    * Feat/config database (#1)
    
    * Update project with last version
    
    * Add config for database
    
    * Add User entity
    
    * Add Topic entity
    
    * Add Post entity
    
    * Add Comment entity
    
    * Add all repository
    
    * Add description for topic and fix user error
    
    * Feat/auth (#2)
    
    * Add security
    
    * Add endpoints for register
    
    * Synchro database
    
    * Add enpoint for login
    
    * Add endpoint for get user
    
    * Add endpoint for modify user
    
    * Add endpoint for create post
    
    * Modify endpoint for create post
    
    * Modify between repository & service
    
    * Add endpoint for get all posts
    
    * Add endpoint for get post by id

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.mddapi.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "USERS")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "SUBSCRIPTION", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "topic_id"))
+    private List<Topic> topics;
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/model/dto/UserLoginDTO.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.mddapi.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserLoginDTO {
+
+    String identifier;
+
+    String password;
+    
+}
