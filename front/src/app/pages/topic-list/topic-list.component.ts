cfb66165c1dec2fc3292648fbd91d55cf70271c7
Marie Metay <mariemetay.pro@gmail.com>
2024-06-05 18:34:39 +0200

    Add topic list

--- front/src/app/pages/topic-list/topic-list.component.ts
+++ front/src/app/pages/topic-list/topic-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Topic } from 'src/app/interfaces/response/topic.interface';
+import { AuthService } from 'src/app/services/auth.service';
+import { TopicService } from 'src/app/services/topic.service';
 
 @Component({
   selector: 'app-topic-list',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicListComponent implements OnInit {
 
-  constructor() { }
+  topics$ = this.topicService.getTopics()
+
+  constructor(private topicService: TopicService, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  subscribe(topic: Topic) {
+    console.log("submit")
+    this.topicService.subscribe(topic).subscribe({
+      next: (response) => {
+        console.log("ok")
+      },
+      error: error => console.log(error)
+    });
+  }
 }
---
+++ front/src/app/services/topic.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Topic } from '../interfaces/response/topic.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TopicService {
+
+  private pathService = 'api/topic';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getTopics(): Observable<Topic[]> {
+    return this.httpClient.get<Topic[]>(this.pathService);
+  }
+
+  public subscribe(topic: Topic): Observable<void> {
+    return this.httpClient.put<void>(`${this.pathService}/subscribe`, topic)
+  }
+}
