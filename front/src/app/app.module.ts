a016517730116ff5b0bb4b23a47ca560c5801ce6
Marie Metay <mariemetay.pro@gmail.com>
2024-06-05 13:43:15 +0200

    Add interceptor

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,10 +14,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { ConnectionComponent } from './pages/connection/connection.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PostListComponent } from './pages/post-list/post-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TopicListComponent } from './pages/topic-list/topic-list.component';
 import { AccountComponent } from './pages/account/account.component';
 import { PostFormComponent } from './pages/post-form/post-form.component';
+import { JwtInterceptor } from './configuration/jwt.interceptor';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ConnectionComponent, PostListComponent, TopicListComponent, AccountComponent, PostFormComponent],
@@ -31,9 +33,12 @@ import { PostFormComponent } from './pages/post-form/post-form.component';
     MatIconModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatGridListModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
