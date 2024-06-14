6f9b52276fee22fe3f275835135f4d7ee2c8d540
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-06-14 11:22:47 +0200

    Develop (#23)
    * Feat/post (#3) (#4)
    
    * Feat/config database (#1)
    
    * Update project with last version
    
    * Add config for database
    
    * Add User entity
    
    * Add Topic entity
    
    * Add Post entity
    
    * Add Comment entity
    
    * Add all repository
    
    * Add description for topic and fix user error
    
    * Feat/auth (#2)
    
    * Add security
    
    * Add endpoints for register
    
    * Synchro database
    
    * Add enpoint for login
    
    * Add endpoint for get user
    
    * Add endpoint for modify user
    
    * Add endpoint for create post
    
    * Modify endpoint for create post
    
    * Modify between repository & service
    
    * Add endpoint for get all posts
    
    * Add endpoint for get post by id
    
    * Modify login for connect with only email (#5)
    
    * Modify register to valid password (#6)
    
    * Fix/auth me (#7)
    
    * Modify update user
    
    * Add password update
    
    * Modify get only posts subscribe (#8)
    
    * Feat/comment (#9)
    
    * Add endpoint for create a comment
    
    * Add endpoint for fetch all comments for a post
    
    * Feat/topic (#10)
    
    * Add endpoint for fetch all topics
    
    * Add endpoint for subscribe
    
    * Add endpoint for unsubscribe
    
    * Feat/auth view (#12)
    
    * Add baseUrl for environment
    
    * Modify home page
    
    * Add header
    
    * Add log view
    
    * Adapte login / signup view
    
    * Link back & front
    
    * Add login & signup flow
    
    * Feat/post view (#13)
    
    * Add header for dashboard
    
    * Add header for dashboard small screen (menu view)
    
    * Add sidebar
    
    * Add button to create a post and endpoint for create it
    
    * Add form post
    
    * Add interceptor
    
    * Simplify endpoints get topics
    
    * Add topic list
    
    * Add disable button when user already subscribed
    
    * Add post form
    
    * Add post list
    
    * Add detail post view and endpoint to add comment
    
    * Add comment for post
    
    * Modify some endpoints
    
    * Feat/me view (#14)
    
    * Add account view
    
    * Implement design for account view
    
    * Implement design for detail post view
    
    * Add AuthGuard for route (#15)
    
    * Fix bug create article (#16)
    
    * Perf/refresh view (#17)
    
    * Refresh view when the user subscribe to a topic
    
    * Refresh view when the user unsubscribe to a topic
    
    * Refresh view when the user update info
    
    * Refresh view when the user add comment
    
    * Fix display date by replace createdAt by created_at (#18)
    
    * Perf/responsive (#19)
    
    * Add grid responsive to one column for post list
    
    * Add grid responsive to one column for topic list
    
    * Add grid responsive to one column for account subscribe
    
    * Fix z-index for sidebar
    
    * Fix textarea for comment on post
    
    * Fix responsive comments
    
    * Perf/simplify endpoints (#20)
    
    * Add javadoc dependency
    
    * Add javadoc for auth & add error for save user
    
    * Logout the user when email modified
    
    * Add verification for valid token
    
    * Add javadoc for comment
    
    * Add javadoc for post
    
    * Add javadoc for topic
    
    * Add space between date en auteur for post list
    
    * Add javadoc for all functions
    
    * Modify design card
    
    * Add card component for topic list and account (#22)

--- front/src/app/pages/topic-list/topic-list.component.ts
+++ front/src/app/pages/topic-list/topic-list.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Topic } from 'src/app/interfaces/response/topic.interface';
-import { AuthService } from 'src/app/services/auth.service';
-import { TopicService } from 'src/app/services/topic.service';
 
 @Component({
   selector: 'app-topic-list',
@@ -10,39 +7,8 @@ import { TopicService } from 'src/app/services/topic.service';
 })
 export class TopicListComponent implements OnInit {
 
-  topics$ = this.topicService.getTopics()
-  user$ = this.authService.getUser()
-  userTopic: Topic[] = []
-  breakpoint!: number
-
-  constructor(private topicService: TopicService, private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.user$.subscribe({
-      next: (user) => {
-        this.userTopic = user.topics
-      }
-    })
-
-    this.breakpoint = (window.innerWidth <= 500) ? 1 : 2
-  }
-
-  subscribe(topic: Topic) {
-    this.topicService.subscribe(topic).subscribe({
-      next(value) {
-          window.location.reload()
-      },
-      error(err) {
-          console.log(err)
-      },
-    });
-  }
-
-  isSubscribed(topicId: number) {
-    return this.userTopic.find((topic) => topic.id === topicId)
-  }
-
-  onResize(event: any) {
-    this.breakpoint = (event.target.innerWidth <= 500) ? 1 : 2
   }
 }
